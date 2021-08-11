697d85953331ea591610280a0dc6522ed4ac71c2
Mannycjr <manny@mycjr.com>
2021-08-11 00:02:14 -0700

    Added automatic player movement to learn transform.Translate

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // take the current position = new position
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * 5 * Time.deltaTime);
+    }
+}
