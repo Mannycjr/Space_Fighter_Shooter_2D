84e557847edb5dd1a45b99b34bbf3ac71ab75df4
Mannycjr <manny@mycjr.com>
2021-08-20 16:39:24 -0700

    added player input movement control for player game object

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 3.5f;
+    [SerializeField]
+    private float _speed = 3.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,39 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        float _horizontalInput = Input.GetAxis("Horizontal");
+        float _verticalInput = Input.GetAxis("Vertical");
+        float _verticalPositionLimit = 3.8f;
+        float _horizontalPositionLimit = 11.30f;
+
+        Vector3 _direction = new Vector3(_horizontalInput, _verticalInput, 0);
+
+        transform.Translate(_direction * _speed * Time.deltaTime);
+
+        // if player position on the y is greater than 3.8
+        // y position = 3.8
+        // else if player position on the y is less than 3.8
+        // y position = -3.8
+
+        if (transform.position.y >= _verticalPositionLimit)
+        {
+            transform.position = new Vector3(transform.position.x, _verticalPositionLimit, 0); 
+        } else if (transform.position.y <= -_verticalPositionLimit)
+        {
+            transform.position = new Vector3(transform.position.x, -_verticalPositionLimit, 0);
+        }
+
+        // if player on the x > 11
+        // x pos = -11
+        // else if player on the x is less than -11
+        // x pos = 11
+        if (transform.position.x >= _horizontalPositionLimit)
+        {
+            transform.position = new Vector3(-_horizontalPositionLimit, transform.position.y, 0);
+        } else if (transform.position.x <= -_horizontalPositionLimit)
+        {
+            transform.position = new Vector3(_horizontalPositionLimit, transform.position.y, 0);
+        }
+
     }
 }
