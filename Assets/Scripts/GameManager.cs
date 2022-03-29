1f138145efcc30db999d0d782abe2ef47a12eb21
Mannycjr <manny@mycjr.com>
2022-03-29 07:51:45 -0700

    Added escape key to quit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,6 +16,12 @@ public class GameManager : MonoBehaviour
         {
             SceneManager.LoadScene(1); //Current Game Scene
         }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
     }
 
     public void GameOver()
